02c8ec6c3ffe37eea027ab1cb3f55128101ac59c
Oyedare <ooyedaretemiloluwa@gmail.com>
2022-09-01 01:28:03 +0100

    header change

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,8 +4,8 @@ import { StyledHeader } from '../styles/Header.styled'
 export const Header = () => {
   return (
     <StyledHeader>
-        <p>I'm from Nigeria</p>
-        <h1>A Frontend Developer</h1>
+      <p>Hello👋🏾, I'm</p>
+      <h1>Temiloluwa</h1>
     </StyledHeader>
   )
 }
