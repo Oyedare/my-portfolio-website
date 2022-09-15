359c7f3f6584fe5d14a6bd3d200f1fe8f09cae33
Oyedare <ooyedaretemiloluwa@gmail.com>
2022-09-15 17:00:54 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalFont } from './fonts/font';
 import { GlobalStyle } from './styles/GlobalStyles';
-import { ResetStyle } from './styles/Reset';
+// import { ResetStyle } from './styles/Reset';
 // import './font.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ResetStyle />
+    {/* <ResetStyle /> */}
     <GlobalFont />
     <GlobalStyle />
     <App />
